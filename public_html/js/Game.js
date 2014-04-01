760835a2191be19fc64f7003e109864e945a0225
Jason J. <support@ovrhere.com>
2014-04-01 19:52:14 -0400

    [Mod]: Completed basic game logic
    Includes the sequence flashing, the feedback, the reset, the 
    autoprogress, and scores.

--- public_html/js/Game.js
+++ public_html/js/Game.js
@@ -1,34 +1,84 @@
-if (!HexamemEngine){
+if (typeof HexamemEngine === 'undefined'){
     HexamemEngine = function(){};
 }
 /**
- * Creates a new game with an empty game.
- * @version 0.1.0-20140326
+ * Creates a new game with an empty list.
+ * 
+ * @param {HexamemEngine.prototype.PlayArea} gameArea The game board area where 
+ * the game is played
+ * * @param {HexamemEngine.prototype.HeaderBoard} scoreBoard The score board where
+ * scores and controls are.
+ * @version 0.2.0-20140401
  * @author Jason J.
- * @type Game
- * @returns {Game}
+ * @type HexamemEngine.prototype.Game
+ * @returns {HexamemEngine.prototype.Game}
  */
-HexamemEngine.prototype.Game = function(){
-
-    /** @type Array|Number the pattern list of the current game. */
+HexamemEngine.prototype.Game = function(gameArea, scoreBoard){
+    /** @type HexamemEngine.prototype.PlayArea The play area that is displayed. */
+    var gameboard = gameArea;
+    /** @type HexamemEngine.prototype.HeaderBoard The score board area. */
+    var scoreboard = scoreBoard;
+    
+    /** @type Array|Number The pattern/position list of the current game. */
     var currentGame = new Array();
-    /** @type Number The index of the user's current play.  */
+    /** @type Number The (zero indexed) index of the user's current play.  */
     var currentPlay = 0;
+    /** @type Number The game diffculty. Cannot be larger than FLASH_DELAY */
+    var currentDifficulty = 0;
+    
+    /** @type Number The current score. */
+    var currentScore = 0;
+    /** @type Number The best score. */
+    var bestScore = 0;
+    
     /** @type The current board size. */
-    var boardSize = 0;
+    var boardSize = gameArea.getSize();
     
     /** @type Number The limit on how many times a number can be repeated in row. */
-    var REPEAT_LIMIT = 4;
+    var REPEAT_LIMIT = 3;
     /** @type Number The auto progress time. */
-    var AUTO_PROGRESS_TIME = 1750;
+    var AUTO_PROGRESS_TIME = 3550;
+    /** @type Number the delay before providing feedback. */
+    var FEEDBACK_DELAY = 1000;
+    
+    /** @TODO game difficulty slope. */
+    /** @TODO local storage of best scores. */
+    
+    /** @type Array|Number The flash speed to use. Values are 0 - 3. */
+    var FLASH_SPEED = [0,       0,      1,          1,      2,      2];
+    /** @type Array|Number A list of delays for between the flashes.*/
+    var FLASH_DELAY = [800+300, 800+100, 600+200, 600+100, 400+200, 400+100];
+    //first number signifies the flash itself.
     
     /** @type boolean Whether or not to auto progress. Default true. */
-    var autoProgress = true;    
+    var autoProgress = scoreboard.getAutoProgress();    
+    
+    ////////////////////////////////////////////////////////////////////////////
+    //// End Members set.
+    ////////////////////////////////////////////////////////////////////////////
+    updateScores();
+    scoreboard.setAutoProgressListener(function(auto){
+        autoProgress = auto;
+    });
+    
+    gameboard.setMiddleButton('Start', 1);
+    gameboard.setMiddleButtonListener(function(){
+        progress(true);
+    });
+    var that = this;
+    gameboard.setFlashButtonListener(function(pos){
+        that.testMove(pos);
+    });
+    
+    scoreboard.setResetListener(function(){
+        gameboard.setMiddleButton('Start', 1);
+        that.reset();
+    });
     
     ////////////////////////////////////////////////////////////////////////////
     //// interal functions
     ////////////////////////////////////////////////////////////////////////////
-    /** Tests choice to see if valid. 
+    /** Tests choice to see if valid.  Do not call directly.
      * @param {Number} choice The choice of the user 
      * @return {Boolean} <code>true</code> for valid, <code>false</code> otherwise.*/
     var testChoice = function(choice){
@@ -36,14 +86,18 @@ HexamemEngine.prototype.Game = function(){
             return false;
         }
         //returns if the choice matched the move and increments to the next play.
-        return choice === currentGame[currentPlay++];
+        if (choice === currentGame[currentPlay]){
+            currentPlay++; //only increment if we are good to go.
+            return true;
+        }
+        return false;
     };
     
     /** @return {Number} Randomly regenerated number from 1 - boardSize */
-    function regen(){ return (Math.random() % boardSize) + 1; }
+    function regen(){ return (Math.floor(Math.random() * boardSize + 1)); }
     
-    /** Generates the next game move. 
-     * Ensuring there are no more than 'repeatLimit' repeats. */
+    /** Generates the next game move in the squence and adds it.
+     * Ensuring there are no more than 'REPEAT_LIMIT' repeatitions. */
     function generateNext(){        
         var nextMove = regen();
         var LIMIT = currentGame.length - REPEAT_LIMIT;
@@ -67,21 +121,81 @@ HexamemEngine.prototype.Game = function(){
         currentGame.push(nextMove);
     }    
     
-    function flashSequence(){
-        
+    /**<b>Deprecated</b>
+     * Flashes the button at the give position with appropriate difficulty. 
+     * @param {Number} position the button to flash. 
+     * @param {Number} delay The delay before the */
+    function flash(position, delay){
+        console.log('flash:'+position)
+        setTimeout(
+                function(){
+                    gameboard.flashPosition(position, FLASH_SPEED[currentDifficulty]);
+            }, delay);
     }
     
-    function progress(){
-        generateNext();
-        if (autoProgress){
-            setTimeout(flashSequence, AUTO_PROGRESS_TIME);
+    /** Flashes the current sequence recursively.
+     * @param {Number} index (Optional) The index of the game to flash. Default is 0.*/
+    function flashSequence(index){
+        if (typeof index !== 'number'){
+            index = 0;
+        }
+        if (0 === index){//disable buttons on first round.
+            gameboard.enableFlashButtons(false);
+            gameboard.setMiddleButton('', 0);
+        }
+        console.log('seq: %s --> %s', index, currentGame[index])
+        gameboard.flashPosition(currentGame[index], FLASH_SPEED[currentDifficulty]);
+        index++;        
+        if (currentGame.length === index){
+            //we are done.
+            gameboard.enableFlashButtons(true);
         } else {
-            /** @TODO continue button. */
+             setTimeout(
+                        function(){flashSequence(index);},
+                        FLASH_DELAY[currentDifficulty]);
         }
     }
     
+    /** Progresses the game to next level. 
+     * @param {boolean} force if true forces the progress. */
+    function progress(force){
+        console.log('progreess')
+        gameboard.enableFlashButtons(false);
+        generateNext();
+        if (force){
+            flashSequence();
+        } else if (autoProgress ){
+            setTimeout(flashSequence, AUTO_PROGRESS_TIME);
+        } else {
+            gameboard.setMiddleButton('Continue?', 1);
+        }
+    }
+    /** Updates the high score and current score. */
+    function updateScores(){
+        currentScore = currentGame.length;
+        if (bestScore < currentScore){
+            scoreboard.updateBestScore(bestScore = currentScore);
+        }
+        scoreboard.updateCurrentScore(currentScore);
+    }
+    /** Provides the user with feedback for this round. *
+     * @param {Boolean} good true if good, false if bad.
+     */
     function roundFeedback(good){
-       
+        gameboard.enableFlashButtons(false);
+        if (good){
+            setTimeout(function(){
+                gameboard.positiveFeedback();
+                updateScores();
+            }, FEEDBACK_DELAY);
+        } else {
+            var currPlay = currentGame[currentPlay];
+            //gameboard.positiveFeedback();
+            gameboard.negativeFeedback();
+            gameboard.setMiddleButton('Retry?', 2);
+            //show correct move.
+            gameboard.longFlashPosition(currPlay);            
+        }
     };
     ////////////////////////////////////////////////////////////////////////////
     //// Public functions
@@ -89,10 +203,13 @@ HexamemEngine.prototype.Game = function(){
     this.reset = function(){
         currentGame = new Array();        
         currentPlay = 0;
+        currentScore = 0;
+        updateScores();
     };
     
     this.start = function(){
-        generateNext();        
+        this.reset();
+        progress(true);
     };
     
     this.setAutoProgress = function(autoprogress){
