fc876be7d728b6a43403dd81766c17c69b274245
Jason J. <support@ovrhere.com>
2014-04-01 19:57:19 -0400

    Added middle button, updated animations + index + engine
    [Mod]: Added the middle button styles to the basic game layout
    [Fix]: Fixed typo in the animation style of feedbacks.
    [Mod]: Updated animation 'shake' and the length of time for
    super-slow fades. 
    [Mod][Add]: For index:
    -Added the includes of the javascript,
    -Updated title 
    -added middle button to DOM (and click area)
    -Added meta data for ipad/touch screen use to prevent zoom

--- public_html/js/hexamem-engine.js
+++ public_html/js/hexamem-engine.js
@@ -1,18 +1,40 @@
 /** The first file to be included for the  game to function. */
 
-
-if (!HexamemEngine){
-    HexamemEngine = function(){};
+var DEBUG_MODE_ON = false;
+if (typeof console === 'undefined'){
+    console = {
+        log:function(s){
+            if (DEBUG_MODE_ON){
+                alert(s);
+            }
+        }
+    };
 }
-
+/** The super object to contain the entire game engine. 
+     * 
+     * @version 0.2.0-20140401
+     * @author Jason J.
+     * @type hexamemEnginer|Object
+     */
+function HexamemEngine (){
+    try {
+        var gameboard = new HexamemEngine.prototype.PlayArea(6);
+        var scoreboard = new HexamemEngine.prototype.HeaderBoard();
+        var game = new HexamemEngine.prototype.Game(gameboard, scoreboard);        
+    } catch (e){
+        console.log('Error: '+e);
+    }
+};
 function startYourEngines(){
     /** The super object to contain the entire game engine. 
      * 
-     * @version 0.1.0-20140327
+     * @version 0.1.0-20140328
      * @author Jason J.
      * @type hexamemEnginer|Object
      */
     hexamemEngine = new HexamemEngine();
+    //Prevent scrolling on ipads
+    window.addEventListener('touchmove',function(e) {e.preventDefault();},false);
 }
 
 if (window.addEventListener){
